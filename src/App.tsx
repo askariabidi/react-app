fea451b1cef908ff8acb1accc2b7f423be112e6e
Syed Mohammad Askari Abidi <askariabbasabidi110@gmail.com>
2023-05-07 03:03:41 +0500

    Created a Form
    Just 2 inputs and a submit button

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div>
-      <ExpandableText maxChars={5}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex maxime quod
-        quam dicta praesentium a recusandae, eligendi hic consequatur atque,
-        aliquam fugit necessitatibus molestias pariatur, sed corporis tempore ea
-        expedita ut quisquam molestiae fuga nesciunt aspernatur. Eum distinctio
-        dolor dolorum molestiae error animi quisquam quaerat, iusto cum aperiam
-        illum, odit velit numquam maiores. Doloremque odio assumenda
-        consequuntur omnis, cupiditate ut nulla, ipsum maiores hic magni
-        exercitationem! Quis eius velit, fugit obcaecati vero, tenetur, corporis
-        libero dolorem deleniti suscipit incidunt temporibus culpa odit itaque
-        et quia dicta pariatur. Magnam suscipit ratione perspiciatis. Alias sint
-        dolorem odit nam error doloremque corporis amet!
-      </ExpandableText>
+      <Form></Form>
     </div>
   );
 }
